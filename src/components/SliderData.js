e6d37515cb61cd87d4da85c0b25d9d1ec68f4adc
Teodora Radovic <tradovic19@gmail.com>
2022-01-29 22:47:54 +0100

    Dodata stranica galerija

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -2,11 +2,21 @@ import img1 from "../assets/bar.jpg";
 import img2 from "../assets/smoothiecokolada.jpg";
 import img3 from "../assets/cokoladamalina.jpg";
 
+import slika1 from "../galerija/slika1.jpg";
+import slika2 from "../galerija/slika2.jpg";
+import slika3 from "../galerija/slika3.jpg";
+import slika6 from "../galerija/slika6.jpg";
+import slika7 from "../galerija/slika7.jpg";
+import slika10 from "../galerija/slika10.jpeg";
+import slika11 from "../galerija/slika11.jpg";
+import slika13 from "../galerija/slika13.jpeg";
+import slika14 from "../galerija/slika14.jpg";
+
 export const SliderData = [
 
 {
 
-image: img1
+image: slika3
 
 },
 
@@ -20,7 +30,49 @@ image: img1
 
         image: img3
         
-        }
+        },
+
+        {
+
+            image: slika6
+            
+            },
+
+            {
+
+                image: slika7
+                
+                },
+
+                {
+
+                    image: slika10
+                    
+                    },
+
+                    {
+
+                        image: slika11
+                        
+                        },
+
+                        {
+
+                            image: slika13
+                            
+                            },
+
+                            {
+
+                                image: slika14
+                                
+                                }
+        
+    
+
+
+        
+    
 
 
 
